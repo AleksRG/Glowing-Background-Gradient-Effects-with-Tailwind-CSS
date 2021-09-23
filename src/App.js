f097f1bc98fd4946daf47d33d9c845171b4f6bae
Aleksandar Georgiev <aleksandar.kystendil.georgiev@gmail.com>
2021-09-23 17:29:37 +0300

    clear unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="min-h-screen px-8 py-96 bg-black">
+      <div class="grid gap-8 items-start justify-center">
+        <div class="relative group">
+          <div class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 via-yellow-600 to-purple-600 rounded-lg filter blur-lg opacity-75 group-hover:opacity-100 transition duration-200"></div>
+          <button class="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+            <span class="flex items-center space-x-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="text-pink-600 transform -rotate-45 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span class="pr-6 text-gray-100">Let's go PAPAFAM 2021</span>
+            </span>
+            <span class="pl-6 text-indigo-400 group-hover:tet-gray-100 transition duration-200">
+              See what's new &rarr;
+            </span>
+          </button>
+        </div>
+      </div>
+    </body>
   );
 }
 
